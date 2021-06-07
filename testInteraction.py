dad845bcdd24f4d38e7c91a70e40eb667e8319cf
Meredith Thomas <meredityman@gmail.com>
2021-06-07 21:15:14 +0200

    Added test intereactions script

---
+++ testInteraction.py
@@ -0,0 +1,30 @@
+from app.interaction import engine
+
+
+ids = [ 0 ]
+
+config = {
+    'public-type': 'none',
+    'private-type': 'survey',
+    'ids': ids,
+} 
+
+def send_message(message):
+    print(message)
+
+def send_private_message(id, message):
+    print(id, message)
+
+
+if __name__ == '__main__':
+
+    engine.setup(config, send_message, send_private_message)
+    
+    while True:
+        try:
+
+            message = input()
+            engine.feedEnginePrivate(0, 0, message)
+
+        except KeyboardInterrupt:
+            break
